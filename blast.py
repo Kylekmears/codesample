31507c91ba82c935d3d9cc62f8236f17c2227099
kylekmears <kylekmears@gmail.com>
2016-06-23 08:22:40 +0000

    final product

--- blast.py
+++ blast.py
@@ -15,6 +15,7 @@ import sqlite3
 import os
 from Bio import SeqIO
 from Bio.Blast import NCBIWWW, NCBIXML
+from datetime import datetime
 
 def blast_to_database(in_file, out_database):
     '''Runs NCBI BLAST query against nr and stores results in an sqlite database'''
@@ -22,6 +23,7 @@ def blast_to_database(in_file, out_database):
     for index, fasta_sequence in enumerate(SeqIO.parse(in_file, 'fasta')):
 
         print('Blasting sequence', index + 1)
+        date = str(datetime.now())
         blast_record = NCBIXML.read(NCBIWWW.qblast('blastn', 'nr', fasta_sequence.seq))
 
         summary_title = fasta_sequence.description
@@ -30,7 +32,6 @@ def blast_to_database(in_file, out_database):
         best_hit_score = 100
 
         for record in range(len(blast_record.descriptions)):
-            print(blast_record.descriptions[record].title)
 
             top_percent = 0
             sum_len = 0
@@ -52,14 +53,14 @@ def blast_to_database(in_file, out_database):
                 best_hit_score = e_val
                 best_hit = description
             
-            result = [description, run_id, percent_id_top_HSP, percent_id_all_HSP, e_val]
+            result = [description, run_id, percent_id_top_HSP, percent_id_all_HSP, e_val, date]
             result_to_database(out_database, result)
 
         num_hits = len(blast_record.descriptions)
         if num_hits == 0:
             print('No results found')
             
-        summary = [summary_title, run_id, num_hits, best_hit, best_hit_score]
+        summary = [summary_title, run_id, num_hits, best_hit, best_hit_score, date]
         summary_to_database(out_database, summary)
 
 def make_database(out_database):
@@ -74,7 +75,8 @@ def make_database(out_database):
                            run_id TEXT, 
                            num_hits INTEGER,
                            top_hit TEXT,
-                           top_hit_e_value REAL)
+                           top_hit_e_value REAL,
+                           date TEXT)
                    """)
 
         db.execute("""
@@ -83,7 +85,8 @@ def make_database(out_database):
                            run_id TEXT,
                            percent_id_top_HSP REAL,
                            percent_id_all_HSP REAL,
-                           e_value REAL)
+                           e_value REAL,
+                           date TEXT)
                    """)
 
 
@@ -92,21 +95,20 @@ def make_database(out_database):
     except Exception as e:
         print('raised' + e + 'when trying to connect to database')
 
-# Split in two - summary_to_database and results_to_database
+
 def summary_to_database(out_database, summary):
-    '''Takes in summary list and sends data to database
-        summary = [title, run id, num hits, best hit, best hit score]'''
+    '''Takes in summary list of results and sends data to database
+        overview = [title, run id, num hits, best hit, best hit score]'''
 
     connection = sqlite3.connect(out_database)
     db = connection.cursor()
 
     db.execute("""
                INSERT into summary
-               (title, run_id, num_hits, best_hit, best_hit_score)
-               values
-               ({}, {}, {}, {}, {})
-               """).format(summary[0], summary[1], summary[2]
-                           summary[3], summary[4])
+               (title, run_id, num_hits, top_hit, top_hit_e_value, date)
+               values(?, ?, ?, ?, ?, ?)
+               """, summary
+              )
 
     connection.commit()
 
@@ -119,22 +121,22 @@ def result_to_database(out_database, result):
     db.execute("""
                INSERT into results
                (description, run_id, percent_id_top_HSP, 
-                percent_id_all_HSP, e_value)
-               values({}, {}, {}, {}, {})
-               """).format(result[0], result[1], result[2],
-                           result[3], result[4])
+                percent_id_all_HSP, e_value, date)
+               values(?, ?, ?, ?, ?, ?)
+               """, result
+              )
 
     connection.commit()
 
 def main():
 
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group()
     
     parser.add_argument("input", help = "Path to fasta-format file containing \
                         DNA sequences", type = str)
     parser.add_argument("-o", "--output", help =
-                        "Specify path to output database", type = str)
+                        "Specify path to output database (directories \
+                         in path must already exist)", type = str)
 
     args = parser.parse_args()
 
@@ -145,6 +147,7 @@ def main():
     
     make_database(out_db)
     blast_to_database(args.input, out_db)
+    print('Complete. Results sent to', out_db)
         
 if __name__ == "__main__":
     main()
